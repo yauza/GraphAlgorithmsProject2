76c1185a966939272ecd4f672f25b388c99f4ad5
yauza <61622378+yauza@users.noreply.github.com>
2021-02-09 11:34:43 +0100

    start

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    while(T--){
+        int N, M;
+        scanf("%d %d", &N, &M);
+
+        for(int i = 0; i < M; i++){
+            int a, b;
+            scanf("%d %d", &a, &b);
+        }
+    }
+
+
+    return 0;
+}
